b49ddddcf994eeaa7181269450178d89b3ac555e
Shobith45 <shobith@robosoftin.com>
2021-09-30 09:19:41 +0530

    saving and restoring state

--- app/src/main/java/net/contriverssoft/activitystate/MainActivity.kt
+++ app/src/main/java/net/contriverssoft/activitystate/MainActivity.kt
@@ -9,11 +9,25 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var counterTextView: TextView
     private var countValue = 0
+    companion object {
+        const val SCORE_KEY = "score_key"
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         counterTextView = findViewById(R.id.tv_count);
+        displayCounter()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putInt(SCORE_KEY,countValue)
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        countValue = savedInstanceState.getInt(SCORE_KEY,0)
         displayCounter()
     }
 
